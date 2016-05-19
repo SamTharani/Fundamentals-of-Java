d842ca94c1146a52bd7e1c7e62ea51f478642c82
Samantha Jeyakumar <samanthaj@hsenidmobile.com>
2016-05-19 09:58:49 +0530

    Refactoring code

--- src/com/fundamental/examples/Main.java
+++ src/com/fundamental/examples/Main.java
@@ -13,47 +13,47 @@ public class Main {
         Scanner num = new Scanner(System.in);
         System.out.print("Enter the range of numbers = ");
         int n = num.nextInt();
-     //   int i = 0 , pr = 0;
-      //  String writePrime = "";
+        int i = 0, pr = 0;
+        String writePrime = "";
         List<Integer> primeList = new ArrayList();
 
-        /*for(i=1;i<n;i++){
+        for (i = 1; i < n; i++) {
             int inc = 0;
-            for(pr=i;pr>=1;pr--){
-                if(i%pr==0){
+            for (pr = i; pr >= 1; pr--) {
+                if (i % pr == 0) {
                     inc = inc + 1;
                 }
             }
-            if(inc==2){
-                writePrime = writePrime + i+ " ";
+            if (inc == 2) {
+                writePrime = writePrime + i + " ";
             }
         }
-        System.out.print(writePrime);*/
+        System.out.print(writePrime);
 
 
         //Method 2 =====>Print prime numbers in a list
 
-        /*for(i=1;i<n;i++){
+        for (i = 1; i < n; i++) {
             int inc = 0;
-            for(pr=i;pr>=1;pr--){
-                if(i%pr==0){
+            for (pr = i; pr >= 1; pr--) {
+                if (i % pr == 0) {
                     inc = inc + 1;
                 }
             }
-            if(inc==2){
+            if (inc == 2) {
                 primeList.add(i);
             }
         }
-        for(int k=0;k<primeList.size();k++){
-            System.out.print(primeList.get(k)+" ");
-        }*/
+        for (int k = 0; k < primeList.size(); k++) {
+            System.out.print(primeList.get(k) + " ");
+        }
 
 
         //Method 3=====> Using prime class print prime
-        Prime primeRange = new Prime(n,primeList);
+        Prime primeRange = new Prime(n, primeList);
         List<Integer> outputList = primeRange.printPrime();
-        for(int k=0;k<outputList.size();k++){
-            System.out.print(primeList.get(k)+" ");
+        for (int k = 0; k < outputList.size(); k++) {
+            System.out.print(primeList.get(k) + " ");
         }
 
 
